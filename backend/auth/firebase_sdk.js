4216d6e6ba4f4f7d9b65eb606b006b0cc8ff18bf
milanPatel001 <mp28238@gmail.com>
2023-09-23 13:48:59 -0400

    fixed private key parsing

--- backend/auth/firebase_sdk.js
+++ backend/auth/firebase_sdk.js
@@ -4,7 +4,9 @@ module.exports = {
   type: "service_account",
   project_id: "stockx-655e8",
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined,
   client_email: "firebase-adminsdk-cdol5@stockx-655e8.iam.gserviceaccount.com",
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
