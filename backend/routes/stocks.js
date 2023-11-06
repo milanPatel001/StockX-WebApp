a33f7af89b017b425fec5ab9cb59387f20e134ca
milanPatel001 <mp28238@gmail.com>
2023-11-06 12:34:16 -0500

    fixed api formatting v2

--- backend/routes/stocks.js
+++ backend/routes/stocks.js
@@ -132,9 +132,9 @@ router.get("/growthTechStocks", requestCache, async (req, res) => {
     .request(opt)
     .then(function (response) {
       const cacheKey = req.originalUrl;
-      redisClient.SETEX(cacheKey, 3600, JSON.stringify(response.data.quotes));
+      redisClient.SETEX(cacheKey, 3600, JSON.stringify(response.data.body));
 
-      res.set(options).send(response.data.quotes);
+      res.set(options).send(response.data.body);
     })
     .catch(function (error) {
       console.error(error);
